02069b0a08193b22e6906d036fe1a9139e3236c5
Isaac Priestley <progressions@gmail.com>
2017-12-18 17:02:21 -0600

    Use %Node notation when pattern matching node maps

--- lib/binary_search_tree.ex
+++ lib/binary_search_tree.ex
@@ -17,9 +17,9 @@ defmodule BinarySearchTree do
 
   """
   def insert(nil, data), do: %Node{data: data}
-  def insert(%{data: node_data}=node, data) when data > node_data, do:
+  def insert(%Node{data: node_data}=node, data) when data > node_data, do:
     %Node{data: node.data, left: node.left, right: insert(node.right, data)}
-  def insert(%{data: node_data}=node, data) when data < node_data, do:
+  def insert(%Node{data: node_data}=node, data) when data < node_data, do:
     %Node{data: node.data, left: insert(node.left, data), right: node.right}
   def insert(node, _), do: node
 
@@ -124,17 +124,17 @@ defmodule BinarySearchTree do
   Find an element in the tree with the given data.
 
   """
-  def find(%{data: data}=node, data), do: node
+  def find(%Node{data: data}=node, data), do: node
   def find(nil, _), do: nil
-  def find(node, data) do
-    find(node.left, data) || find(node.right, data)
+  def find(%Node{left: left, right: right}, data) do
+    find(left, data) || find(right, data)
   end
 
   @doc """
   Find the element with the next smallest data given a node and the root node.
 
   """
-  def next_smallest(%{left: node}, _) when is_map(node), do: largest(node)
+  def next_smallest(%Node{left: node}, _) when is_map(node), do: largest(node)
   def next_smallest(node, root), do: next_smallest(node, root, nil)
   defp next_smallest(node, root, successor) do
     cond do
@@ -151,7 +151,7 @@ defmodule BinarySearchTree do
   Find the element with the next largest data given a node and the root node.
 
   """
-  def next_largest(%{right: node}, _) when is_map(node), do: smallest(node)
+  def next_largest(%Node{right: node}, _) when is_map(node), do: smallest(node)
   def next_largest(node, root), do: next_largest(node, root, nil)
   defp next_largest(node, root, successor) do
     cond do
@@ -168,15 +168,15 @@ defmodule BinarySearchTree do
   Find the element with the largest data in the entire tree.
 
   """
-  def largest(%{right: nil}=node), do: node
-  def largest(%{right: node}), do: largest(node)
+  def largest(%Node{right: nil}=node), do: node
+  def largest(%Node{right: node}), do: largest(node)
 
   @doc """
   Find the element with the smallest data in the entire tree.
 
   """
-  def smallest(%{left: nil}=node), do: node
-  def smallest(%{left: node}), do: smallest(node)
+  def smallest(%Node{left: nil}=node), do: node
+  def smallest(%Node{left: node}), do: smallest(node)
 
   @doc """
   Delete a given element from the tree and move its children into place.
@@ -186,10 +186,10 @@ defmodule BinarySearchTree do
   """
   def delete(root, data), do: delete(nil, root, data)
   defp delete(acc, nil, _), do: acc
-  defp delete(acc, %{data: data, left: nil, right: nil}, data), do: acc
-  defp delete(acc, %{data: data, left: nil, right: right}, data), do: delete(acc, right, data)
-  defp delete(acc, %{data: data, left: left, right: nil}, data), do: delete(acc, left, data)
-  defp delete(acc, %{data: data, left: left, right: right}, data) do
+  defp delete(acc, %Node{data: data, left: nil, right: nil}, data), do: acc
+  defp delete(acc, %Node{data: data, left: nil, right: right}, data), do: delete(acc, right, data)
+  defp delete(acc, %Node{data: data, left: left, right: nil}, data), do: delete(acc, left, data)
+  defp delete(acc, %Node{data: data, left: left, right: right}, data) do
     %{data: d} = smallest(right)
 
     insert(acc, d)
@@ -220,9 +220,9 @@ defmodule BinarySearchTree do
   """
   def sum(node), do: sum(0, node)
   defp sum(acc, nil), do: acc
-  defp sum(acc, node) do
-    acc + node.data
-    |> sum(node.left)
-    |> sum(node.right)
+  defp sum(acc, %Node{data: data, left: left, right: right}) do
+    acc + data
+    |> sum(left)
+    |> sum(right)
   end
 end
