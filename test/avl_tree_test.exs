0ad74326eb5eafda8e3a158a28d352e46350d995
Isaac Priestley <progressions@gmail.com>
2017-12-22 12:00:03 -0600

    2 is the threshold

--- test/avl_tree_test.exs
+++ test/avl_tree_test.exs
@@ -10,18 +10,16 @@ defmodule AvlTreeTest do
 
   test "insert_with_height needs balancing on left" do
     {_, height} = AvlTree.insert(nil, 1)
-    |> AvlTree.insert(2)
-    |> AvlTree.insert_with_height(3)
+    |> AvlTree.insert_with_height(2)
 
-    assert height == 3
+    assert height == 2
   end
 
   test "insert_with_height needs balancing on right" do
     {_, height} = AvlTree.insert(nil, 3)
-    |> AvlTree.insert(2)
-    |> AvlTree.insert_with_height(1)
+    |> AvlTree.insert_with_height(2)
 
-    assert height == 3
+    assert height == 2
   end
 
 end
